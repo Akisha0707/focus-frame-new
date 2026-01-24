76f1a2836f6302e2cc343f16109609389b859efe
Nata <Sakimova1983@gmail.com>
2026-01-24 17:50:33 +0200

    changes js files

--- src/index.js
+++ src/index.js
@@ -1,52 +1,64 @@
+// add alt for img
+const getImg = document.querySelector('.foto-section-reviews');
+getImg.setAttribute('alt', 'women');
 
-// const getImg = document.querySelector(".foto-section-reviews")
-// getImg.setAttribute("alt", "women")
-// console.log(getImg.getAttribute("alt"))
-// console.log(getImg)
+// push button
+const getButton = document.querySelector('.button-section-sign-up');
 
-// const getButton = document.querySelector(".button-section-sign-up")
-
-// getButton.addEventListener("click",(event)=>{
-// event.preventDefault()
-// console.log (event)
-// console.log ("Button was click")
-// })
-// console.log(getButton)
-
-// const handClick = (event)=> {
-//     console.log (event)
-// }
-
-// const getNavMenu = document.querySelector(".svg-nav-menu")
-// const getMobileMenu = document.querySelector(".mobile-menu-landing")
-
-// const getSum = (event) => { 
-//     event.preventDefault();
-//     getMobileMenu.style.display = "flex";
-//     console.log("done")
-// }
-// getNavMenu.addEventListener("click", getSum)
-
-
-// const closeMobileMenu = document.querySelector(".svg-x")
-
-// const closeMobile = (event) => {
-//     event.preventDefault();
-//     getMobileMenu.style.display = "none";
-// }
-
-// closeMobileMenu.addEventListener("click", closeMobile)
-"user strict"
-
-const petya = {
-  username: "Petya",
-  showThis() {
-    console.log(this);
-  },
-  showName() {
-    console.log(this.username);
-  },
+const handClick = event => {
+  event.preventDefault();
+  console.log(event);
+  console.log('Button was click');
 };
 
-petya.showThis(); // {username: "Petya", showThis: ƒ, showName: ƒ}
-petya.showName(); // 'Petya'
+getButton.addEventListener('click', handClick);
+
+// push svg
+const getNavMenu = document.querySelector('.svg-nav-menu');
+const getMobileMenu = document.querySelector('.mobile-menu-landing');
+
+const getSum = event => {
+  event.preventDefault();
+  getMobileMenu.style.display = 'flex';
+  console.log('done');
+};
+getNavMenu.addEventListener('click', getSum);
+
+// close svg
+const closeMobileMenu = document.querySelector('.svg-x');
+
+const closeMobile = event => {
+  event.preventDefault();
+  getMobileMenu.style.display = 'none';
+};
+
+closeMobileMenu.addEventListener('click', closeMobile);
+
+// keydown
+const keydownOn = event => {
+  event.preventDefault();
+  console.log(event);
+  if (event.key !== 'Enter') {
+    return;
+  } else {
+    console.log('hello');
+  }
+};
+
+document.addEventListener('keydown', keydownOn);
+
+// form elements
+const select = document.querySelector('.pizza-select');
+const textOutput = document.querySelector('.text-output');
+const valueOutput = document.querySelector('.value-output');
+
+select.addEventListener('change', setOutput);
+
+function setOutput(event) {
+  const selectedOptionValue = event.currentTarget.value;
+  const selectedOptionIndex = event.currentTarget.selectedIndex;
+  const selectedOptionText = event.currentTarget.options[selectedOptionIndex].text;
+
+  textOutput.textContent = selectedOptionText;
+  valueOutput.textContent = selectedOptionValue;
+}
---
+++ src/select.js
@@ -0,0 +1,30 @@
+// const getImg = document.querySelector(".foto-section-reviews")
+// getImg.setAttribute("alt", "women")
+// console.log(getImg.getAttribute("alt"))
+// console.log(getImg)
+// const getButton = document.querySelector(".button-section-sign-up")
+// getButton.addEventListener("click",(event)=>{
+// event.preventDefault()
+// console.log (event)
+// console.log ("Button was click")
+// })
+// console.log(getButton)
+// const handClick = (event)=> {
+//     console.log (event)
+// }
+// const getNavMenu = document.querySelector(".svg-nav-menu")
+// const getMobileMenu = document.querySelector(".mobile-menu-landing")
+// const getSum = (event) => {
+//     event.preventDefault();
+//     getMobileMenu.style.display = "flex";
+//     console.log("done")
+// }
+// getNavMenu.addEventListener("click", getSum)
+// const closeMobileMenu = document.querySelector(".svg-x")
+// const closeMobile = (event) => {
+//     event.preventDefault();
+//     getMobileMenu.style.display = "none";
+// }
+// closeMobileMenu.addEventListener("click", closeMobile)
+'user strict';
+const select;
